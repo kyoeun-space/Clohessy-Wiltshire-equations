a26b14bfa72ebc61d7466cb6076a8f619e9d61a2
kyoeun <122524815+kyoeun-space@users.noreply.github.com>
2024-05-10 19:48:34 +0900

    ko -> en

--- Clohessy-Wiltshire-Equations.m
+++ Clohessy-Wiltshire-Equations.m
@@ -14,15 +14,15 @@ fx = x_ddot(t) - 2*w*y_dot(t) - 3*(w^2)*x(t);
 fy = y_ddot(t) + 2*w*x_dot(t);
 fz = z_ddot(t) + (w^2)*z(t);
 
-% fy 적분
+% integrate fy 
 integral_fy = int(fy, t);
 integral_fy_equation = integral_fy == C;
 
-% y 항 삭제
+% delete y term
 pre_x = 2*w*integral_fy + fx;
 simplify_pre_x = simplify(pre_x);
 
-%% ==== x 식 계산 ==== %%
+%% ==== calculate x ==== %%
 % x(t) = ~
 x_equation = isolate(dsolve(simplify_pre_x == 2*w*C, t) == x(t), x(t));
 % x(0) = ~
@@ -32,7 +32,7 @@ x_dot_equation = diff(x_equation, t);
 % x'(0) = ~
 x0_dot_equation = subs(x_dot_equation, t, 0);
 
-%% ==== x 식 계산 ==== %%
+%% ==== calculate y ==== %%
 % y'(t) = ~~
 cal_y_dot = integral_fy_equation - 2*w*x_equation;
 y_dot_equation = simplify(expand(cal_y_dot));
@@ -43,7 +43,7 @@ y_equation = isolate(rewrite(dsolve(y_dot_equation, t), "exp") == y(t), y(t));
 % y(0) = ~
 y0_equation = subs(y_equation, t, 0);
 
-%% ==== 적분 상수 계산 ==== %%
+%% ==== find constant C ==== %%
 % C = ~
 val_C = isolate(expand(y0_dot_equation + (2*w*x0_equation)), C);
 C3pC4 = expand(-2/w * (x0_equation * w * (3/2) + y0_dot_equation));
@@ -55,7 +55,7 @@ val_C3 = isolate(subs((1/2) * (C3pC4 + C3nC4), C5, rhs(val_C5)), C3);
 % C4 = ~
 val_C4 = isolate(subs((1/2) * (C3pC4 - C3nC4), C5, rhs(val_C5)), C4);
 
-%% ==== z 식 계산 ==== %%
+%% ==== calculate z ==== %%
 % z(t) = ~
 z_equation = isolate(subs(dsolve(fz==0) == z(t), [C3, C4], [C6, C7]), z(t));
 % z'(t) = ~
@@ -65,14 +65,14 @@ z0_equation = subs(z_equation, t, 0);
 % z'(0) = ~
 z0_dot_equation = subs(z_dot_equation, t, 0);
 
-%% ==== 적분 상수 계산 ==== %%
+%% ==== find constant C ==== %%
 % C6 = ~
 val_C6 = isolate((1/(2*w*1i)) * expand(z0_equation*w*1i - z0_dot_equation), C6);
 % C7 = ~
 val_C7 = isolate((1/(2*w*1i)) * expand(z0_equation*w*1i + z0_dot_equation), C7);
 
-%% ==== 적분 상수 대입 ==== %%
-% 적분 상수
+%% ==== substitute constants into euqations  ==== %%
+% constants
 coefficients = [C, C3, C4, C5, C6, C7];
 values = [rhs(val_C), rhs(val_C3), rhs(val_C4), rhs(val_C5), rhs(val_C6), rhs(val_C7)];
 
@@ -95,7 +95,7 @@ obtain_y_dot_equation = expand(rewrite(cal_y_dot_equation, "sincos"));
 cal_z_dot_equation = subs(z_dot_equation, coefficients, values);
 obtain_z_dot_equation = expand(rewrite(cal_z_dot_equation, "sincos"));
 
-%% ==== 초기값 대입 ==== %%
+%% ==== substitute initial conditions into equations ==== %%
 zero_coefficients = [w, x(0), y(0), z(0), subs(diff(x(t), t), t, 0), subs(diff(y(t), t), t, 0), subs(diff(z(t), t), t, 0)];
 initial_values_1 = [0.001107, 500, 0, 0, 0, 0, 0]; % m
 initial_values_2 = [0.001107, 0, 0, 0, 0, 0.1, 0]; % km/s or 1.4539e-08 rad/s || D*ω/2
@@ -108,7 +108,7 @@ x_equation_2 = subs(obtain_x_equation, zero_coefficients, initial_values_2);
 y_equation_2 = subs(obtain_y_equation, zero_coefficients, initial_values_2);
 z_equation_2 = subs(obtain_z_equation, zero_coefficients, initial_values_2);
 
-%% ==== 시간 설정 ==== %%
+%% ==== set the time ==== %%
 t_values = linspace(0, 10000, 1000);
 
 x_values_1 = subs(x_equation_1, t, t_values);
@@ -119,7 +119,7 @@ x_values_2 = subs(x_equation_2, t, t_values);
 y_values_2 = subs(y_equation_2, t, t_values);
 z_values_2 = subs(z_equation_2, t, t_values);
 
-%% ==== 그래프 ==== %%
+%% ==== plot ==== %%
 figure;
 subplot(2, 3, 1);
 plot(t_values, rhs(x_values_1));
